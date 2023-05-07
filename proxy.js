b31e3b8f28a0d0f7560578ff8fbfa076721a8123
acklenx <github.com@qlamail.com>
2023-05-07 10:46:57 -0400

    removing auth requirement

--- proxy.js
+++ proxy.js
@@ -1,10 +1,10 @@
 const express = require( 'express' ),
 	request = require( 'request' ),
-	fucors  = express();
+	fucors = express();
 
 fucors.all( '*', function( req, res, next )
 {
-	console.log("request received");
+	console.log( "request received" );
 	// Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
 	res.header( "Access-Control-Allow-Origin", "*" );
 	res.header( "Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE" );
@@ -23,38 +23,17 @@ fucors.all( '*', function( req, res, next )
 		}
 		else
 		{
-			const authKey = req.header( 'qla' );
-			console.log( "authKey", authKey );
-			if( !authKey )
-			{
-				res.send( 500, { error: "FU!  -hugs and kisses, CORS" } );
-			}
-			else if( authKey.length !== 7 )
-			{
-				if( authKey.length < 10 )
+			console.log( "req.url", req.url );
+			request(
+				{ url: targetURL + req.url, method: req.method, json: req.body },
+				function( error, response, body )
 				{
-					res.send( 500, { error: "FU! API key too short. (hint: they are at least 10 alphanumeric characters" } );
-				}
-				else
-				{
-					res.send( 500, { error: "FU! Bad API key. Has it expired?" } );
-				}
-			}
-			else
-			{
-
-				console.log( "req.url", req.url );
-				request(
-					{ url: targetURL + req.url, method: req.method, json: req.body },
-					function( error, response, body )
+					if( error )
 					{
-						if( error )
-						{
-							console.error( 'error: ' + response.statusCode );
-						}
+						console.error( 'error: ' + response.statusCode );
 					}
-				).pipe( res );
-			}
+				}
+			).pipe( res );
 		}
 	}
 } );
